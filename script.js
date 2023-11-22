ab926bcf1814abf49087be992b31528c7e1add92
Jorge Wagner <jorgetrabalho2021@gmail.com>
2023-11-21 21:21:37 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function verificar_caracteres(){
-    if(caracteres_senha.length < 8){
+    if(caracteres_senha.length < 0){ // mudar para < 8 depois
         alert("Sua senha deve conter mais de 8 digitos!")
 
     } else if (caracteres_senha.length > 15) {
@@ -17,10 +17,36 @@ function armazena_senha(){
 function verifica_letras_maiusculas(){
     caracteres_maiusculos = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     for(j = 0; j < caracteres_maiusculos.length; j++){
-        if(caracteres_senha[j][j] == caracteres_maiusculos[j]){  // Desenvolvendo metodo para verificar as letras maiusculas;
-            alert("Sua senha tem letras maisculas")              // Não concluido
+        if(caracteres_senha[caracteres_senha.length - 1][k] == caracteres_maiusculos[j]){  // Desenvolvendo metodo para verificar as letras maiusculas;
+            alert("Sua senha tem letras maisculas")
+            controlMaiusculas == true  
+            break        // Fase de teste da função
         } else {
-            alert("teste")
+            console.log("K = " + k);
+            console.log(caracteres_senha[caracteres_senha.length - 1][k])
+            console.log(caracteres_maiusculos[j])
         }
+    }     
+    verificarParte2_caracteresMaiusculas()
+}
+
+function verificarParte2_caracteresMaiusculas(){
+    k++
+    while(caracteres_senha[caracteres_senha.length - 1][k] != caracteres_maiusculos[j]){
+        console.log("k = " + k);
+        if(controlMaiusculas == false){
+            for(j = 0; j < caracteres_maiusculos.length; j++){
+                if(caracteres_senha[caracteres_senha.length - 1][k] == caracteres_maiusculos[j]){  // Desenvolvendo metodo para verificar as letras maiusculas;
+                    alert("Sua senha tem letras maisculas") // Fase de teste da função
+                    break
+                } else {
+                    controlMaiusculas == false
+                    console.log(caracteres_senha[caracteres_senha.length - 1][k])
+                    console.log(caracteres_maiusculos[j])
+                }
+            }
+        }
+        k++
     }
+   
 }
--- script_captura_dados.js
+++ script_captura_dados.js
@@ -4,3 +4,5 @@ let caracteres_senha = []
 let valorSenhaContrl = 0
 let valorSenhaContrl2 = 0
 let i = 0
+let k = 0
+let controlMaiusculas = false
